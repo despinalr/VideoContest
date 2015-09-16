274dd4149f70d650b68b3c269fa5a221fa73cbd9
David Espinal Ruiz <david.espinal@bizagi.com>
2015-09-16 13:22:22 -0500

    edit fixes

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,18 +1,18 @@
 class ContestsController < ApplicationController
 	def show
-		@idContest = params[:id]
-		@contests_filter = Contest.order(created_at: :desc).where(id: params[:id])
+		@contests_filter = Contest.order(created_at: :desc).where(url: params[:id])
 
 		if @contests_filter.count > 0
 			@current_contest = @contests_filter[0]
+			@idContest = @current_contest.id
 		end
 
 		@contests = []
 
 		if session[:id] != nil
-			@videos = Video.order(created_at: :desc).where(contest_id: params[:id])
+			@videos = Video.order(created_at: :desc).where(contest_id: @idContest)
 		else
-			@videos = Video.order(created_at: :desc).where(contest_id: params[:id], estado: 1)
+			@videos = Video.order(created_at: :desc).where(contest_id: @idContest, estado: 1)
 		end
 	end
 
@@ -22,13 +22,14 @@ class ContestsController < ApplicationController
 	end
 
 	def destroy
-		Video.where(contest_id: params[:contest][:id]).destroy_all
-		Contest.find(params[:contest][:id]).destroy
+        Video.where(contest_id: params[:contest][:id]).destroy_all
+        Contest.find(params[:contest][:id]).destroy
+
 		redirect_to root_path
 	end
 
 	def edit
-		contest = Contest.find_by(params[:id])
+		contest = Contest.find(params[:contest][:id])
 		contest.nombre = params[:contest][:nombre]
 		contest.url = params[:contest][:url]
 		contest.fechainicio = params[:contest][:fechainicio]
