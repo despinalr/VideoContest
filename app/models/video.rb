c4e5b28898cd155adecce2931b2b3f184074736a
JohnR <john.rodriguez@bizagi.com>
2015-09-18 10:09:05 -0500

    Updated url path in paperclip  config

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ActiveRecord::Base
 
-	has_attached_file :video
+	has_attached_file :video,
+	:url => "/system/videos/:id/:filename"
 	validates_attachment_content_type :video, content_type: /\Avideo\/.*\Z/
 
 end
